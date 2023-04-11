c4939e462fe0cb78163ad23381287798b1c2560b
QuCo84 <58950636+QuCo84@users.noreply.github.com>
2023-04-11 09:32:11 +0200

    Update udajax.js

--- app/editor-view/udajax.js
+++ app/editor-view/udajax.js
@@ -54,6 +54,8 @@
         // 2DO add service unless already present and replace for name change
         if ( uri.charAt(0) == '/') this.url = this.server+uri;
         else this.url = this.server+'/'+uri;
+        // Some tools have webdesk wired in there requests
+        uri = uri.replace( '/webdesk/', '/'+this.service+'/');
         
        /* PATCH GCP*/
        if ( method == "POST" && ( strpos( this.server, "appspot") || strpos( this.server, "cloudshell"))) this.url = this.server;
