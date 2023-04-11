552f94a1f97e6c5929a4fd0141a2cf2714816461
QuCo84 <58950636+QuCo84@users.noreply.github.com>
2023-04-11 10:39:10 +0200

    Update sdbee-index.php

--- app/sdbee-index.php
+++ app/sdbee-index.php
@@ -259,7 +259,7 @@ function SDBEE_getRequest() {
     $requestKeys = array( 
         'test', 'e', 'form', 'nServiceRequest', 'task', 'model', 'collection', 'act', 'input_oid', 
         'nname', 'nlabel', 'stype', 'nstyle', 'tcontent', 'thtml', 'nlang', 'textra', 'ngivenname', 'nParams', 'lastTime', 'ticks',
-        'tpasswd', 'tdomain', '_stype', '_tdomain'
+        'tpasswd', 'tdomain', '_stype', '_tdomain', 'ttext'
     );
     foreach( $requestKeys as $key) {
         if ( isset( $_REQUEST[ $key])) $request[ $key] = $_REQUEST[ $key];
